259877cb7b41720287891fedf03acba80c936b82
Gengiskan <91074795+ForMyFreedom@users.noreply.github.com>
2022-01-15 11:23:36 -0300

    Create SkillGUI

---
+++ New Era/source/guis/SkillGUI.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class SkillGUI : WindowDialog
+{
+    public override void _Ready()
+    {
+        
+    }
+
+    public void PopupIt()
+    {
+        PopupCenteredRatio(0.5f);
+    }
+}
