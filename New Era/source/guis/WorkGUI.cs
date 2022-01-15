e101915d87d73ad5a1ce531b420a6e0b80e2f3d2
Gengiskan <91074795+ForMyFreedom@users.noreply.github.com>
2022-01-15 11:23:54 -0300

    Amplify WorkGUI

--- New Era/source/guis/WorkGUI.cs
+++ New Era/source/guis/WorkGUI.cs
@@ -4,49 +4,85 @@ using System;
 public class WorkGUI : WindowDialog
 {
     [Export]
-    private PackedScene workPackedScene;
-    [Export]
     private NodePath descriptionLabelPath;
     [Export]
     private NodePath rollBoxPath;
+    [Export]
+    private NodePath workTexture;
 
-    private Work work;
-    private int workLevel;
+    [Signal]
+    public delegate void value_changed(int index, int value);
 
+    private Work work = null;
+    private int workIndex;
+    
 
     public override void _Ready()
     {
-
-        work = workPackedScene.Instance<Work>();
         WindowTitle = work.GetWorkName();
 
         GetNode<Label>(descriptionLabelPath).Text = work.GetDescription();
+        GetNode<TextureRect>(workTexture).Texture = work.GetBaseImage();
+
         GetNode(rollBoxPath).Connect("roll_maded", this, "_OnRollMaded");
         GetNode(rollBoxPath).Connect("value_changed", this, "_OnValueChanged");
+        Connect("popup_hide", this, "_OnPopupHide");
     }
 
 
     private void _OnRollMaded(int result)
     {
-
+        //@
     }
 
     private void _OnValueChanged(int value)
     {
+        EmitSignal(nameof(value_changed), workIndex, value);
+    }
 
+
+    public void ConnectAllSignals(WorkTree main)
+    {
+        Connect(nameof(value_changed), main, nameof(main._OnWorkValueChanged));
+    }
+
+
+    private void _OnPopupHide()
+    {
+        QueueFree();
     }
 
 
 
+
+    public void PopupIt()
+    {
+        PopupCenteredRatio(0.5f);
+    } 
+
+    public void SetWork(Work w)
+    {
+        work = w;
+    }
+
+    public int GetWorkIndex()
+    {
+        return workIndex;
+    }
+
+    public void SetWorkIndex(int index)
+    {
+        workIndex = index;
+    }
 
 
     public int GetLevelValue()
     {
-        return workLevel;
+        return GetNode<RollBox>(rollBoxPath).GetRollValue();
     }
 
     public void SetLevelValue(int value)
     {
-        workLevel = value;
+        GetNode<RollBox>(rollBoxPath).SetRollValue(value);
     }
 }
