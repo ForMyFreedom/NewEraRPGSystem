fdf211b995c02cafedcab1685855af919834f9d3
Gengiskan <91074795+ForMyFreedom@users.noreply.github.com>
2022-01-20 20:19:51 -0300

    Develop the base of Notification Area Code

--- New Era/source/NotificationArea.cs
+++ New Era/source/NotificationArea.cs
@@ -7,12 +7,49 @@ public class NotificationArea : Control
     private NodePath bellPath;
     [Export]
     private NodePath animationPath;
+    [Export]
+    private NodePath notificationListPath;
+    [Export]
+    private NodePath quantNotificationsPath;
+
+    [Export]
+    private Texture blankTexture;
 
     private bool toShow = false;
+    ItemList notificationList;
 
     public override void _Ready()
     {
+        notificationList = GetNode<ItemList>(notificationListPath);
         GetNode(bellPath).Connect("gui_input", this, "_OnBellGuiInput");
+        GetNode(notificationListPath).Connect("gui_input", this, "_OnNotificationListGuiInput");
+    }
+
+
+    public void CreateNewNotification(String message, Texture texture=null)
+    {
+        if (texture == null) texture = blankTexture;
+        notificationList.AddItem(message, texture);
+        GetNode<AnimationPlayer>(animationPath).Play("new_notification");
+        ActualizeQuantNotificationsLabel();
+    }
+
+    public void DeleteNotification(int index)
+    {
+        if (index < 0) return;
+        notificationList.RemoveItem(index);
+        ActualizeQuantNotificationsLabel();
+    }
+
+
+    private void _OnNotificationListGuiInput(InputEvent @event)
+    {
+        if (!(@event is InputEventMouseButton)) return;
+        InputEventMouseButton buttonEvent = (InputEventMouseButton)@event;
+        if (buttonEvent.Doubleclick)
+        {
+            DeleteNotification(TryGetTheSelectedNotificationIndex());
+        }
     }
 
 
@@ -31,5 +68,34 @@ public class NotificationArea : Control
             GetNode<AnimationPlayer>(animationPath).Play("show");
         else
             GetNode<AnimationPlayer>(animationPath).Play("dishow");
+    }
+
+
+
+    private void ActualizeQuantNotificationsLabel()
+    {
+        int quant = notificationList.Items.Count/3;
+        if (quant > 0)
+        {
+            GetNode<Label>(quantNotificationsPath).Visible = true;
+            GetNode<Label>(quantNotificationsPath).Text = quant.ToString();
+        }
+        else
+        {
+            GetNode<Label>(quantNotificationsPath).Visible = false;
+        }
+    }
+
+
+    private int TryGetTheSelectedNotificationIndex()
+    {
+        try
+        {
+            return notificationList.GetSelectedItems()[0];
+        }
+        catch (Exception)
+        {
+            return -1;
+        }
     }
 }
