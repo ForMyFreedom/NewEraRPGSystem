a863fce41c9ea199f8905aa4c849bd19ccad67b2
Gengiskan <91074795+ForMyFreedom@users.noreply.github.com>
2022-01-23 19:51:48 -0300

    Add Medic Correct FirstUpStep

--- New Era/source/works/Medic.cs
+++ New Era/source/works/Medic.cs
@@ -6,6 +6,7 @@ public class Medic : Work
     public override void DoFirstUpStep(MainInterface gui)
     {
         gui.AddMind(1);
+        gui.AddSomeSkillLevel(5, 0, enumWork);
     }
 
     public override void DoSecondUpStep(MainInterface gui)
