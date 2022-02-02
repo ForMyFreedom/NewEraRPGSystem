087ff58bfe8a0526e6ea32e286102e75c23c48f6
Gengiskan <jiyuunotameni@hotmail.com>
2022-02-02 09:10:24 -0300

    Create Test CriticUse

---
+++ New Era/source/habilitys/critic-uses/Azazel/Navigatozao.cs
@@ -0,0 +1,10 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public class Navigatozao : CriticUse
+{
+    public override void DoMechanic(MainInterface main, int actionIndex = 0)
+    {
+    }
+}
