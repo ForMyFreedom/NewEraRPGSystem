80c09031ed467ec775f91e90743dd0cc16aec59e
Gengiskan <jiyuunotameni@hotmail.com>
2022-03-06 11:30:14 -0300

    Organize Folders

---
+++ New Era/source/static/global/Global.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class Global : Node
+{
+    [Export]
+    private PackedScene selectedPlayerPacked;
+
+
+    public void SetSelectedPlayerPacked(PackedScene packed)
+    {
+        selectedPlayerPacked = packed;
+    }
+
+
+    public PackedScene GetSelectedPlayerPacked()
+    {
+        return selectedPlayerPacked;
+    }
+
+    public String GetSelectedSavePlayerFolder()
+    {
+        Player player = selectedPlayerPacked.Instance<Player>();
+        return MyStatic.savePath+player.Name;
+    }
+}
