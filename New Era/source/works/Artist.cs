5382b5991c1e0d00b43c3badcd9a71bc1f1459f5
Gengiskan <jiyuunotameni@hotmail.com>
2022-03-13 12:57:27 -0300

    Create Artist Work and TemporalArt Skill

---
+++ New Era/source/works/Artist.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public class Artist : Work
+{
+    [Export]
+    private int baseDamage;
+
+    public override void DoFirstUpStep(MainInterface gui)
+    {
+        gui.AddCharisma(1);
+        gui.AddAnSkillLevel(enumWork, 0, 5);
+        //@ maybe make the skillLevel here a request to distribuite in its variated 'arts'
+    }
+
+    public override void DoSecondUpStep(MainInterface gui)
+    {
+        gui.CreateNewNotification(GetCreateCriticMessage(), baseImage);
+    }
+
+    public override void DoThirdUpStep(MainInterface gui)
+    {
+        gui.CreateNewNotification(GetCreateTechMessage(), baseImage);
+    }
+
+    public override void DoForthUpStep(MainInterface gui)
+    {
+        gui.CreateNewNotification($"Voce alcansou uma maestria de {workName}! " +
+            "Escolha entre: \n" + maestryDescription, baseImage);
+    }
+
+    public override int GetBaseDamage(MainInterface gui, int weaponDamage = 0, int actionIndex = 0)
+    {
+        return baseDamage + gui.GetAtributeNodeByEnum(relationedAtribute).GetAtributeValue()/2;
+    }
+}
