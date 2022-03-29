b74600a000be60b5f215f14570a11a3661f63fee
Gengiskan <jiyuunotameni@hotmail.com>
2022-03-29 18:47:34 -0300

    Create FerraoDaRainha

---
+++ New Era/source/habilitys/critic-uses/Melissa/FerraoDaRainha.cs
@@ -0,0 +1,19 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public class FerraoDaRainha : CriticUse
+{
+    [Export]
+    private Texture customImage;
+
+    public override void DoMechanicLogic(MainInterface main, int actionIndex = 0, int critic = -1)
+    {
+        main.CreateNewNotification(baseMessage, customImage);
+    }
+
+    public override void DoEndMechanicLogic()
+    {
+    }
+
+}
