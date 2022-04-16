dd83eaba360ed38ecc356d87ba467b64a40b1b34
Gengiskan <jiyuunotameni@hotmail.com>
2022-04-16 10:45:32 -0300

    Create null construct

--- New Era/source/update-life/LifeUpdaterAbstract.cs
+++ New Era/source/update-life/LifeUpdaterAbstract.cs
@@ -5,6 +5,7 @@ public abstract class LifeUpdaterAbstract : Godot.Object
 {
     protected MainInterface main;
 
+    public LifeUpdaterAbstract() { }
     public LifeUpdaterAbstract(MainInterface main) => this.main = main;
 
     public abstract void DoUpdateOfLife(int value);
